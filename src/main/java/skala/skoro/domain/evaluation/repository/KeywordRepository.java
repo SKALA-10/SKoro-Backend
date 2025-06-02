49c7eb8cba7cfa1f9676fff752a05bd5a4b729e6
novventa <novventa@kakao.com>
2025-06-02 16:29:55 +0900

    Feat: KeywordRepository 수정

--- src/main/java/skala/skoro/domain/evaluation/repository/KeywordRepository.java
+++ src/main/java/skala/skoro/domain/evaluation/repository/KeywordRepository.java
@@ -3,5 +3,8 @@ package skala.skoro.domain.evaluation.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import skala.skoro.domain.evaluation.entity.Keyword;
 
+import java.util.List;
+
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+    List<Keyword> findAll();
 }
