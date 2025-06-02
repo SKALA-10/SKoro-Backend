43721114fd4e364a1842b97238836d1501e58279
novventa <novventa@kakao.com>
2025-06-02 16:28:28 +0900

    Feat: PeerEvaluationRepository 수정

--- src/main/java/skala/skoro/domain/evaluation/repository/PeerEvaluationRepository.java
+++ src/main/java/skala/skoro/domain/evaluation/repository/PeerEvaluationRepository.java
@@ -3,5 +3,10 @@ package skala.skoro.domain.evaluation.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import skala.skoro.domain.evaluation.entity.PeerEvaluation;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PeerEvaluationRepository extends JpaRepository<PeerEvaluation, Long> {
+    List<PeerEvaluation> findByEmployee_EmpNoAndTeamEvaluation_Period_Id(String empNo, Long periodId);
+    Optional<PeerEvaluation> findById(Long peerEvaluationId);
 }
