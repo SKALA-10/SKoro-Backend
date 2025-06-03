1982ecd8cdcda33c0032b25e635c0c375634362f
KimJ4ehyun <kimjaehyun158@gmail.com>
2025-06-03 10:39:59 +0900

    Feat: 팀의 평가 기간 목록 조회

--- src/main/java/skala/skoro/domain/period/controller/PeriodController.java
+++ src/main/java/skala/skoro/domain/period/controller/PeriodController.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import skala.skoro.domain.period.dto.TeamEvaluationPeriodResponse;
 import skala.skoro.domain.period.service.PeriodService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,7 +16,7 @@ public class PeriodController {
     private final PeriodService periodService;
 
     @GetMapping("/periods")
-    public ResponseEntity<?> getTeamEvaluationPeriods() {
+    public ResponseEntity<List<TeamEvaluationPeriodResponse>> getTeamEvaluationPeriods() {
         return ResponseEntity.ok(periodService.getTeamEvaluationPeriods());
     }
 }
