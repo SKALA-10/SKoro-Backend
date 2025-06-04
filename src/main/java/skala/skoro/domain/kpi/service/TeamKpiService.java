f0b69c8af0e9128361811e15f99a1fb2a4abef8f
KimJ4ehyun <kimjaehyun158@gmail.com>
2025-06-04 17:14:50 +0900

    Feat: 올해 팀 KPI 조회

---
+++ src/main/java/skala/skoro/domain/kpi/service/TeamKpiService.java
@@ -0,0 +1,32 @@
+package skala.skoro.domain.kpi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import skala.skoro.domain.employee.entity.Employee;
+import skala.skoro.domain.employee.service.EmployeeService;
+import skala.skoro.domain.kpi.dto.TeamKpiDetailResponse;
+import skala.skoro.domain.kpi.repository.TeamKpiRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TeamKpiService {
+
+    private final EmployeeService employeeService;
+
+    private final TeamKpiRepository teamKpiRepository;
+
+    public List<TeamKpiDetailResponse> getTeamKpis() {
+        String empNo = "E001"; // TODO
+
+        Employee employee = employeeService.findEmployeeByEmpNo(empNo);
+
+        return teamKpiRepository.findByTeamAndYearOrderByProgressDesc(employee.getTeam(), LocalDate.now().getYear()).stream()
+                .map(TeamKpiDetailResponse::from)
+                .toList();
+    }
+}
