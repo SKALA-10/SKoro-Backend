6452ccebe18da972a0ca7c2b7961cd9f6deeb052
novventa <novventa@kakao.com>
2025-06-04 14:49:46 +0900

    Refactor: 사용하지 않는 코드 삭제

--- src/main/java/skala/skoro/config/RedisConfig.java
+++ src/main/java/skala/skoro/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package skala.skoro.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
