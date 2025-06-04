5b46d3cf082bb1bcd9c553fce2e01cf1bf541d54
novventa <novventa@kakao.com>
2025-06-04 11:44:00 +0900

    Feat: RefreshTokenRequest TokenResponse 추가

---
+++ src/main/java/skala/skoro/domain/auth/dto/TokenResponse.java
@@ -0,0 +1,13 @@
+package skala.skoro.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+}
