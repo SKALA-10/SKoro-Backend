284ab0f31003e9c4cf7fdacaac63d361a2a9637c
novventa <novventa@kakao.com>
2025-06-05 13:56:05 +0900

    Feat: FeedbackReportRepository 분기별 달성률 조회 추가

--- src/main/java/skala/skoro/domain/kpi/dto/MyContributionResponse.java
+++ src/main/java/skala/skoro/domain/kpi/dto/MyContributionResponse.java
@@ -1,5 +1,10 @@
 package skala.skoro.domain.kpi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class MyContributionResponse {
     private Integer year;
     private Integer quarter;
