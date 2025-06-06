a4d8071a173b86689759478c3374ffcad23e759f
KimJ4ehyun <kimjaehyun158@gmail.com>
2025-06-06 19:31:51 +0900

    Refactor: config 패키지 global 하위로 이동

--- src/main/java/skala/skoro/global/config/SecurityConfig.java
+++ src/main/java/skala/skoro/global/config/SecurityConfig.java
@@ -1,6 +1,5 @@
-package skala.skoro.config;
+package skala.skoro.global.config;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
