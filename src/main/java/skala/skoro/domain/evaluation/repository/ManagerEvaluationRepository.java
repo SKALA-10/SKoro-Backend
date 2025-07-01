6d237ee1bfb692931d6627cf07af7127b868be46
KimJ4ehyun <kimjaehyun158@gmail.com>
2025-07-01 14:10:20 +0900

    Fix: ManagerEvaluation 삭제

--- src/main/java/skala/skoro/domain/evaluation/repository/ManagerEvaluationRepository.java
+++
@@ -1,7 +0,0 @@
-package skala.skoro.domain.evaluation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import skala.skoro.domain.evaluation.entity.ManagerEvaluation;
-
-public interface ManagerEvaluationRepository extends JpaRepository<ManagerEvaluation, Long> {
-}
